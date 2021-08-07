8373a0a4cb3c6398f88e000f460e324c42b510c5
KISHOTH-K <73494038+KISHOTH-K@users.noreply.github.com>
2021-08-07 08:28:52 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,5 @@
+import keyword
+
+# printing python keywords
+print("Python keywords are")
+print(keyword.kwlist)
---
+++ string.py
@@ -0,0 +1,52 @@
+a="Python"
+print(a[5])
+print(a[4:])
+w="Saved programs"
+print(w[2:7])
+first="kishoth"[2]
+print(first)
+# string operations
+#String Lower Case
+a="Python is a general purpose language"
+print(a.lower())
+#String Upper Case
+b="Python is used for web development, ai, ml"
+print(b.upper())
+#String length
+print(len(b))
+#Concatenation
+print(b+str(2))
+print("hi"+" "+"bye")
+#replace
+fruit="Kishoth likes grapes,grapes is a fruit"
+print(fruit)
+print(fruit.replace("grapes","apple",1))#fruit.replace("","",count)
+
+text="he was a civil engineer,but he likes to  work as  a  mechanical engineer "
+
+print(text.replace("engineer","stream"))
+print(fruit[1:3])
+print(fruit[1:3:1])
+print(fruit[1:3:2])
+print(fruit[-1])
+print(fruit[::])
+print(fruit[::1])
+print(fruit[::2])
+#Reverse of the string
+print(fruit[::-1])
+y="yellow"
+g="green"
+print("I like"+" "+y+" "+"and"+" "+g)
+print("I like %s and %s"%(y,g))
+
+
+
+
+
+
+
+
+
+
+
+
